2a4c1ff9439fe9e9161b55597e62f4f8ac3fd678
Jaume Singla <jaume.singla@privalia.com>
2018-04-17 13:08:51 +0200

    Preparing the django docker

---
+++ config/postgres/run.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+/usr/sbin/uwsgi --ini /home/server/uwsgi.ini:local
--- test/test.sh
+++ test/test.sh
@@ -2,13 +2,13 @@
 
 tag="finances:test"
 
-docker build . -t $tag -f config/sqlite/Dockerfile
+docker build . -t $tag -f $1 
 
-#docker run -p 80:80 -p 8000:8000 -it $tag bash
-docker run --name finances -p 80:80 -p 8000:8000 $tag supervisord -n
+docker run -p 80:80 -p 8000:8000 -it $tag sh
+#docker run --name finances -p 80:80 -p 8000:8000 $tag supervisord -n
 
 for container in $(docker ps --all | grep "$tag" | cut -d' ' -f1); do
     docker rm $container
 done
 
-echo "docker rmi -f $tag"
+docker rmi -f $tag
