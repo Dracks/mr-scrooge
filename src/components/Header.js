ac1de1c2feb6e71192d93efdd2ea5f699e2d95d0
Jaume Singla <jaume.singlavalls@gmail.com>
2018-05-25 18:38:48 +0200

    Changed the name

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Header = ({logout}) => {
     return (<nav className="green">
         <div className="nav-wrapper">
-            <Link to="/" className="brand-logo">Finances</Link>
+            <Link to="/" className="brand-logo">Mr Scrooge</Link>
             <ul id="nav-mobile" className="right hide-on-med-and-down">
                 <li><Link to='/'>Report</Link></li>
                 <li><Link to='/import'>Import</Link></li>
