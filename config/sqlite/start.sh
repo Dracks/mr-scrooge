fc78373691f4f690ea79c9be9edd09b39e573e20
Jaume Singla <jaume.singlavalls@gmail.com>
2018-06-16 01:03:13 +0200

    Updated start.sh to support configuration of user password

--- config/sqlite/start.sh
+++ config/sqlite/start.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
+if [ "$DEMOUSER" = "" ]; then
+    DEMOUSER="demo"
+fi
+
+if [ "$DEMOPWD" = "" ]; then
+    DEMOPWD="demo"
+fi
+
 python3 /home/django/manage.py migrate
-python3 /home/django/manage.py demouser
+python3 /home/django/manage.py demouser -u $DEMOUSER -p $DEMOPWD
 
 supervisord -n
