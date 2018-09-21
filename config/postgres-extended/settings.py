57986b7774798a532ada87fa0cde1242318f30f0
Jaume Singla <jaume.singlavalls@gmail.com>
2018-09-21 07:32:29 +0200

    Adding a docker with postgres

---
+++ config/postgres-extended/settings.py
@@ -0,0 +1,16 @@
+from .settings_base import * 
+
+
+# Database
+# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mrscrooge',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'postgres',
+        'PORT': '',
+    }
+}
