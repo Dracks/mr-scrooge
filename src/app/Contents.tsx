bbe5da8b0bad0a11ba27a5d4ab39bee24f0c09be
Jaume Singla <jaume.singlavalls@gmail.com>
2018-11-17 12:56:58 +0100

    Now is splitted, and working on development environment

--- src/app/Contents.tsx
+++ src/app/Contents.tsx
@@ -1,11 +1,29 @@
 import * as React from 'react';
+import Loadable from 'react-loadable';
 import { Route, Switch } from 'react-router-dom';
 
-import GraphReportPage from './Pages/GraphReportPage';
-import ImportPage from './Pages/ImportPage';
-import RawDataPage from './Pages/RawDataPage';
-import TagPage from './Pages/TagPage';
+import { CenteredLoading } from '../components/Loading';
 
+
+const GraphReportPage = Loadable({
+    loader: () => import("./Pages/GraphReportPage"),
+    loading: CenteredLoading
+});
+
+const ImportPage = Loadable({
+    loader: ()=> import('./Pages/ImportPage'),
+    loading: CenteredLoading,
+});
+
+const RawDataPage = Loadable({
+    loader: ()=> import('./Pages/RawDataPage'),
+    loading: CenteredLoading,
+});
+
+const TagPage = Loadable({
+    loader: ()=> import('./Pages/TagPage'),
+    loading: CenteredLoading,
+});
 
 const Contents = () => {
     return (
