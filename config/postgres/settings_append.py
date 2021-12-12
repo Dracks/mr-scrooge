08b98a227a710817c455b5486f921542a14a9b5b
Jaume Singla Valls <singlavalls@mediamarktsaturn.com>
2021-12-12 14:21:35 +0100

    Improve docker configuration

--- config/postgres/settings_append.py
+++ config/postgres/settings_append.py
@@ -4,10 +4,10 @@
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'mrscrooge',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'postgres',
-        'PORT': '',
+        'NAME': os.environ.get('DB_NAME', 'mrscrooge'),
+        'USER': os.environ.get('DB_USER', 'postgres'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'postgres'),
+        'HOST': os.environ.get('DB_HOST', 'postgres'),
+        'PORT': os.environ.get('DB_PORT', ''),
     }
 }
