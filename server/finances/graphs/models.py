504ee3ca64acbb0cbfe76a9356421d5a30d32f73
Jaume Singla <jaume.singlavalls@gmail.com>
2022-05-15 18:47:13 +0200

    Preparing a correct DB for the graphs generation

--- server/finances/graphs/models.py
+++ server/finances/graphs/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from finances.management.models import Tag
 
 # Create your models here.
 class Graph(models.Model):
@@ -6,7 +7,55 @@ class Graph(models.Model):
     kind = models.CharField(max_length=50)
     options = models.TextField() # For the moment I will put everything on a json inside here
 
-"""
-class GraphOption(models.Model):
-    graph = models.ForeignKey(Graph, on_delete=models.CASCADE, related_name="options")
-    option = """
+class GraphKind:
+    Bar = 'bar'
+    Pie = 'pie'
+    Line='line'
+
+GraphKind.CHOICES=(
+    (GraphKind.Bar, 'Bar'),
+    (GraphKind.Pie, 'Pie'),
+    (GraphKind.Line, 'Line')
+)
+
+class GraphGroup:
+    Day='day'
+    Month='month'
+    Tags='tags'
+    Sign='sign'  
+
+GraphGroup.CHOICES=(
+    (GraphGroup.Day,'day'),
+    (GraphGroup.Month,'month'),
+    (GraphGroup.Tags,'tags'),
+    (GraphGroup.Sign,'sign' ), 
+)
+
+class GraphV2(models.Model):
+    name = models.CharField(max_length=255)
+    kind = models.CharField(max_length=10, choices=GraphKind.CHOICES)
+    tag_filter = models.ForeignKey(Tag, on_delete=models.PROTECT, null=True, blank=True)
+    # ToDo add choices
+    date_range = models.CharField(max_length=255)
+    # To keep track of the relation with old keys
+    old_graph = models.ForeignKey(Graph, null=True, blank=True, on_delete=models.SET_NULL)
+
+class AbstractGroup(models.Model):
+    group = models.CharField(max_length=10, choices = GraphGroup.CHOICES)
+    hide_others = models.BooleanField(null=True, blank=True)
+    class Meta:
+        abstract=True
+
+class Group(AbstractGroup):
+    graph = models.ForeignKey(GraphV2, on_delete=models.CASCADE)
+
+class GroupTags(models.Model):
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    tag = models.ForeignKey(Tag, on_delete=models.PROTECT)
+
+class HorizontalGroup(AbstractGroup):
+    graph = models.ForeignKey(GraphV2, on_delete=models.CASCADE)
+
+class HorizontalGroupTags(models.Model):
+    group = models.ForeignKey(HorizontalGroup, on_delete=models.CASCADE)
+    tag = models.ForeignKey(Tag, on_delete=models.PROTECT)
--- view2/src/api/client/graphs/types.ts
+++ view2/src/api/client/graphs/types.ts
@@ -1,8 +1,42 @@
 
+
+export enum GraphKind {
+    bar='bar',
+    pie='pie',
+    line='line'
+}
+
+export enum DateRange {
+    all = 'all',
+    twoYears='twoYears',
+    oneYear='year',
+    halfYear='six',
+    oneMonth='month',
+}
+
+export enum GraphGroupEnum {
+    day='day',
+    month='month',
+    tags='tags',
+    sign='sign',
+}
+
 export interface Graph {
     id: number,
     name: string,
-    [k:string]: string | unknown
+    tag: number,
+    kind: GraphKind,
+    dateRange: DateRange,
+    group: GraphGroupEnum
+    groupValue?: number[],
+    groupHideOthers?: boolean,
+}
+
+export interface BarGraph extends Graph{
+    kind: GraphKind.bar | GraphKind.line,
+    horizontal: GraphGroupEnum,
+    horizontalValue?: number[],
+    horizontalHideOthers?: boolean
 }
 
 export type GetGraphsResponse = Graph[]
