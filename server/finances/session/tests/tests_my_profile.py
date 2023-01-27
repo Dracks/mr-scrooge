5374b685abf2cd83cd0896c2635cd6f5b058dabf
jaume <jaume.singlavalls@gmail.com>
2023-01-27 14:46:41 +0100

    update tests to pass correctly

--- server/finances/session/tests/tests_my_profile.py
+++ server/finances/session/tests/tests_my_profile.py
@@ -37,14 +37,16 @@ class MyProfileTest(TestCase):
         self.assertEqual(user.email, data['email'])
 
     def test_patch_password(self):
-        response = self.client.patch('/api/me/', {'new-password': '123456!a', 'password': '123'}, format='json')
+        response = self.client.patch('/api/me/', {'new_password': '123456!a', 'password': '123'}, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         user = User.objects.get(pk=self.user.pk)
+        self.assertTrue(user.check_password(PASSWORD_TEST), 'Check with the correct password')
         self.assertFalse(user.check_password('123456!ab'), "Check with incorrect password")
 
-        response = self.client.patch('/api/me/', {'new-password': '123456!ab', 'password': PASSWORD_TEST}, format='json')
+        response = self.client.patch('/api/me/', {'new_password': '123456!ab', 'password': PASSWORD_TEST}, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         user = User.objects.get(pk=self.user.pk)
+        self.assertFalse(user.check_password(PASSWORD_TEST), 'Check with the old password')
         self.assertTrue(user.check_password('123456!ab'), "Check with correct password")
 
     def test_not_logged(self):
