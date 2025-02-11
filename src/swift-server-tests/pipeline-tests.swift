3d8e0d37ddb4c42ee7a428e850ec0237a7396614
Jaume <jaume.singlavalls@gmail.com>
2025-02-12 00:15:24 +0100

    Simplifying

--- src/swift-server-tests/pipeline-tests.swift
+++ src/swift-server-tests/pipeline-tests.swift
@@ -1,14 +1,58 @@
 import Foundation
+import XCTVapor
 import XCTest
 
 @testable import MrScroogeServer
 
-final class PipelineErrorTests: BaseImporterTests {
-	override func getParsers() throws -> [any ParserFactory] {
+final class PipelineErrorTests: XCTestCase {
+	var importerService: NewImportService!
+	var group: UserGroup!
+	var app: Application?
+
+	override func setUp() async throws {
+		try await super.setUp()
+		do {
+			let app = try await Application.make(.testing)
+			try await configure(app)
+
+			print("Starting auto-migration")
+			try await app.autoMigrate()
+			print("Finishing auto-migration")
+
+			self.app = app
+
+			let testParsers: [ParserFactory] = try getParsers()
+			importerService = NewImportService(parsers: testParsers, withApp: app)
+			print("Set Up Correctly")
+		} catch {
+			print("importer-helper {}", String(reflecting: error))
+			XCTAssertTrue(false)
+		}
+	}
+
+	override func tearDown() async throws {
+		try await super.tearDown()
+		print("Tearing down")
+		guard let app else {
+			throw TestError()
+		}
+		do {
+			try await app.asyncShutdown()
+			print("Async shutdown")
+			self.app = nil
+			print("Finish")
+		} catch {
+			print("More error catching")
+			print(error)
+		}
+	}
+
+	func getParsers() throws -> [any ParserFactory] {
 		return [CommerzBankEnImporter()]
 	}
 
 	func testRegexFile() async throws {
+		print("Test is starting!")
 		let factory = importerService.getParsers().first
 
 		XCTAssertNotNil(factory)
