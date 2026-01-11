4e497f3beff9845f1876f482de15148880e03a92
jaume <jaume.singlavalls@gmail.com>
2026-01-11 07:41:14 +0100

    migrating away of old tools

--- config/setupTests.js
+++ config/setupTests.js
@@ -1,11 +1,8 @@
-/*const Enzyme = require("enzyme")
-const EnzymeAdapter = require("enzyme-adapter-react-16")
 
-Enzyme.configure({ adapter: new EnzymeAdapter() })
-//*/
-//require('reflect-metadata')
 
-jest.mock("../src/react-view/constants", () => ({
+vi.mock(import("../src/react-view/constants"), () => ({
     LOGOUT_URL: "/logout",
     STATIC_URL: "",
+    DEBUG: false,
+    VERSION: "test"
 }))
